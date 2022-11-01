24bb6d6502bc36bb331e66f600abaebbdebe2bc7
marwakhlaifia <khlaifia.marwa1996@gmail.com>
2022-11-01 19:53:03 +0100

    create react state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+//import "./Component/SetInterval";
+import Timer from "./Components/SetInterval";
+import picture from "./Components/marwa.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    fullName: "Marwa Khlaifia",
+    bio: "JS full stack developer.",
+    imgSrc: { picture },
+    show: false,
+  };
+
+  Active = () => {
+    this.setState({
+      show: !this.state.show,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.Active}>Show</button>
+        {this.state.show ? (
+          <div>
+            <h1>{this.state.fullName}</h1>
+            <p>{this.state.bio}</p>
+            <img src={picture} alt="profile" />
+          </div>
+        ) : (
+          <p>Press the button!</p>
+        )}
+        <Timer />
+      </div>
+    );
+  }
 }
 
 export default App;
